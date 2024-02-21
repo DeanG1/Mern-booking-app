6faf398afef28a17f4cc8b5b8f778b48f975fe83
Dido2002 <didoguzelev@gmail.com>
2024-02-21 14:58:50 +0200

    SettingUpLayout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      padding: "8rem",
+    }
   },
+
   plugins: [],
 }
 
